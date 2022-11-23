f9ea25a7bfec279f73b75bd47bf2715dce7216e8
Leonardo Wolf <leonardowolf97@gmail.com>
2022-11-22 21:34:04 -0300

    funcionando

---
+++ src/components/MemeGenerator/MemeGenerator.js
@@ -0,0 +1,157 @@
+import React, { useEffect, useState } from "react";
+import html2canvas from "html2canvas";
+import "./memeGenerator.css";
+import interact from "interactjs";
+import MemeSettings from "../MemeSettings/MemeSettings.js";
+import { getMemes } from "../../services/meme";
+const Spacing = () => {};
+
+const MemeGenerator = () => {
+  const [memeList, setList] = useState([]);
+  const [memeImage, setImage] = useState("");
+  const [memeText, setText] = useState("");
+  const [memeText2, setText2] = useState("");
+  const [memeText3, setText3] = useState("");
+  const [textSize, setTextSize] = useState(22);
+  const [textSize2, setTextSize2] = useState(22);
+  const [textSize3, setTextSize3] = useState(22);
+  const [font, setFont] = useState("arial");
+  const [textColor, setTextColor] = useState("#FFF");
+  const [textColor2, setTextColor2] = useState("#FFF");
+  const [textColor3, setTextColor3] = useState("#FFF");
+  const [showText2, setShowText2] = useState(false);
+  const [showText3, setShowText3] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getMemes();
+        setImage(response[0].url);
+        setList(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const Download = () => {
+    html2canvas(document.querySelector("#export"), {
+      allowTaint: true,
+      useCORS: true,
+      width: 400,
+    }).then(function (canvas) {
+      let img = canvas.toDataURL("memes/jpg");
+      let link = document.createElement("a");
+      link.download = "meme.jpg";
+      link.href = img;
+      link.click();
+    });
+  };
+
+  const position = { x: 0, y: 0 };
+  interact(".draggable").draggable({
+    listeners: {
+      start(event) {},
+      move(event) {
+        position.x += event.dx;
+        position.y += event.dy;
+        event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+      },
+    },
+  });
+
+  return (
+    <>
+      <div className="header">
+        <h1>Generador de memes</h1>
+        <h2 className="titleText">Elegí una imagen</h2>
+        <select onChange={(e) => setImage(e.target.value)}>
+          {memeList.map((meme) => (
+            <option key={meme.id} value={meme.url}>
+              {meme.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="memeGeneratorContainer">
+        <div className="figure">
+          <figure id="export">
+            <figcaption
+              className="memeText draggable"
+              style={{
+                color: `${textColor}`,
+                fontSize: `${textSize}px`,
+                fontFamily: `${font}, serif`,
+              }}
+            >
+              {memeText}
+            </figcaption>
+            <figcaption
+              className="memeText draggable"
+              style={{
+                color: `${textColor2}`,
+                fontSize: `${textSize2}px`,
+              }}
+            >
+              {memeText2}
+            </figcaption>
+            <figcaption
+              className="memeText draggable"
+              style={{
+                color: `${textColor3}`,
+                fontSize: `${textSize3}px`,
+              }}
+            >
+              {memeText3}
+            </figcaption>
+            <img src={memeImage} alt="MemeImage" className="memeImage" />
+          </figure>
+        </div>
+        <div className="memeSettings">
+          <h2 className="titleText">Escribí hasta 3 textos</h2>
+          <p>Arrastralos para ubicarlos en la imagen</p>
+          <MemeSettings
+            textNumber={"Texto 1"}
+            onMemeTextSize={textSize}
+            onSetText={(e) => setText(e.target.value)}
+            onSetTextSize={(e) => setTextSize(e.target.value)}
+            onSetTextColor={(e) => setTextColor(e.target.value)}
+            onSetFont={(e) => setFont(e.target.value)}
+          />
+          {showText2 ? (
+            <MemeSettings
+              textNumber={"Texto 2"}
+              onMemeTextSize={textSize2}
+              onSetText={(e) => setText2(e.target.value)}
+              onSetTextSize={(e) => setTextSize2(e.target.value)}
+              onSetTextColor={(e) => setTextColor2(e.target.value)}
+              onSetFont={(e) => setFont(e.target.value)}
+            />
+          ) : (
+            <button onClick={() => setShowText2(true)}>Agregar texto 2</button>
+          )}
+
+          {showText3 ? (
+            <MemeSettings
+              textNumber={"Texto 3"}
+              onMemeTextSize={textSize3}
+              onSetText={(e) => setText3(e.target.value)}
+              onSetTextSize={(e) => setTextSize3(e.target.value)}
+              onSetTextColor={(e) => setTextColor3(e.target.value)}
+            />
+          ) : (
+            <button onClick={() => setShowText3(true)}>Agregar texto 3</button>
+          )}
+        </div>
+      </div>
+      <div className="header">
+        <button onClick={Download} type="button" className="btnDownload">
+          DESCARGAR MEME
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default MemeGenerator;
---
+++ src/components/MemeSettings/MemeSettings.js
@@ -0,0 +1,46 @@
+const MemeSettings = ({
+  textNumber,
+  textSize,
+  onSetText,
+  onSetTextSize,
+  onSetTextColor,
+  onSetFont,
+}) => {
+  return (
+    <div>
+      <input
+        type="text"
+        className="inputText"
+        onChange={onSetText}
+        placeholder={` ${textNumber}`}
+      />
+
+      <p>Fuente del {textNumber}</p>
+      <div className="fuente">
+        <select name="font" defaultValue="arial" onChange={onSetFont}>
+          <option value="Arial">Arial</option>
+          <option value="Brush Script MT">Brush Script MT</option>
+          <option value="Comic Sans MS">Comic Sans MS</option>
+          <option value="Courier New">Courier New</option>
+          <option value="Garamond">Garamond</option>
+          <option value="Georgia">Georgia</option>
+          <option value="Helvetica">Helvetica</option>
+          <option value="Tahoma">Tahoma</option>
+          <option value="Times New Roman">Times New Roman</option>
+          <option value="Verdana">Verdana</option>
+        </select>
+        <input type="color" onChange={onSetTextColor}></input>
+      </div>
+      <p>Tamaño del {textNumber}</p>
+      <input
+        type="range"
+        min="0"
+        max="72"
+        value={textSize}
+        onChange={onSetTextSize}
+      ></input>
+    </div>
+  );
+};
+
+export default MemeSettings;
